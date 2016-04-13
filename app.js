1e7e3f3f27df4f765a51a8cda0933368c636daf0
spyy <sami.pyy@gmail.com>
2016-04-13 13:21:42 +0000

    ...

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 'use strict'
 
-var ltx = require('node-xmpp-core').ltx
-var PushServer =require('./lib/S2S/PushServer')
+var PushServer = require('./lib/S2S/PushServer')
 var r = new PushServer(5269, '0.0.0.0')
+var debug = require('debug')('push:app')
 
-var rawmsg = "<message to='mu@example.com' from='juliet@nodexmpp.com/balcony' "
-rawmsg = rawmsg + "type='chat' xml:lang='en'><body>Wherefore art thou, mu?</body></message>"
 
 r.register('ec2-52-58-37-243.eu-central-1.compute.amazonaws.com', function (stanza) {
-  console.log('GOT YA << ' + stanza.toString())
-  console.info('GOT YA << ' + stanza.toString())
+  debug(stanza.toString())
   if (stanza.attrs.type !== 'error') {
-    var me = stanza.attrs.to
+    var me = stanza.attrs.to + '/morko'
     stanza.attrs.to = stanza.attrs.from
     stanza.attrs.from = me
-    //r.send(stanza)
   }
 })
 
-var msg = ltx.parse(rawmsg)
